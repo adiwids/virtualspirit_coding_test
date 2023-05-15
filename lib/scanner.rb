c1d58a06fdc42e14ba717611a56404bbe1b81c6d
Adi Widyawan <day4die@gmail.com>
2023-05-15 14:21:45 +0700

    Implement files listing method and store into instance variable.

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,8 +1,19 @@
 class Scanner
-  def initialize; end
+  attr_reader :files
+
+  def initialize
+    @files = []
+  end
 
   def test_cli
     puts 'Welcome!'
+  end
+
+  def scan(dir, ext = 'txt')
+    raise ArgumentError.new("#{dir} directory is not exist") unless Dir.exist? dir
+
+    @files = Dir.glob(File.join(File.expand_path(dir), "*.#{ext}"))
+    raise ArgumentError.new(".#{ext} file not found") if files.empty?
   end
 end
 
