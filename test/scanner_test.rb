ff9fca792c6e495dff7d0a45b7d1da7ae55c4dcd
Adi Widyawan <day4die@gmail.com>
2023-05-15 15:01:02 +0700

    Add test examples for content count case.

--- test/scanner_test.rb
+++ test/scanner_test.rb
@@ -8,6 +8,10 @@ class ScannerTest < Minitest::Test
     @scanner = Scanner.new
   end
 
+  def test_initialize
+    assert @scanner.files.empty?
+  end
+
   def test_cli_output
     assert_output(stdout = /Welcome!/) { @scanner.test_cli }
   end
@@ -24,5 +28,9 @@ class ScannerTest < Minitest::Test
   def test_scan_empty_dir_path
     assert_raises(ArgumentError) { @scanner.scan File.join(@files_dir, 'empty-dir'), 'txt' }
     assert @scanner.files.empty?
+  end
+
+  def test_content_scan_output
+    assert_output( stdout = /[\w+\\r\\n?]+\s(\d+)(\\r\\n)?/) { @scanner.scan @files_dir, 'txt' }
   end
 end
