048dd16fd1e2c2c625a0e426fa83856aa26b5770
Adi Widyawan <day4die@gmail.com>
2023-05-15 15:01:47 +0700

    Implement core file read functionality for small files and content.

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -9,11 +9,31 @@ class Scanner
     puts 'Welcome!'
   end
 
+  # scan homogenous files content and count similar content
+  # Output: {file content} {similar files count}
   def scan(dir, ext = 'txt')
     raise ArgumentError.new("#{dir} directory is not exist") unless Dir.exist? dir
 
     @files = Dir.glob(File.join(File.expand_path(dir), "*.#{ext}"))
     raise ArgumentError.new(".#{ext} file not found") if files.empty?
+
+    count_by_content.each do |content, count|
+      puts "#{content.gsub(/\\\\r\\\\n/, "\r\n")} #{count}"
+    end
+  end
+
+  private
+
+  # TODO: use threaded files reading and temp SQLite database for large file indexing
+  def count_by_content
+    result = {}
+    files.each do |path|
+      content = File.read(path).gsub(/\r\n/, "\\r\\n")
+      result[content] ||= 0
+      result[content] += 1
+    end
+
+    result
   end
 end
 
